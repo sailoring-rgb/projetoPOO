e676619b1b2f63fc91747cbb45ed3d9f936d2f61
AnaHenriques01 <62114404+AnaHenriques01@users.noreply.github.com>
2021-04-02 10:36:24 +0100

    Add files via upload

---
+++ ProjetoPOO/EstadoJogo.java
@@ -0,0 +1,21 @@
+
+/**
+ * Estado jogo.
+ *
+ * @author grupo
+ * @version 210402
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class EstadoJogo
+{
+    private int num_jogadas;
+    private Jogador jogador_atual;
+    private int ultima_jogada;
+    private String nome_equipa;
+    private int num_jogadores;
+    private Jogador jogador;
+    private List<Jogador> equipa;
+}
