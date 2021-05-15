6c0ffc681a087f63802162b1089ecff332bd7d1e
Karlofsky <cardani.santos2@gmail.com>
2021-05-15 16:22:54 +0100

    Adicionei Saver

--- ProjetoPOO/Saver.java
+++ ProjetoPOO/Saver.java
@@ -9,14 +9,75 @@ import java.io.*;
  */
 public class Saver{
     
-    public Saver()
-    {
-
-    }
-
-    /**public void SaveG()
+    public static void save(Data dados)
     {
         
+        Map<String, Equipa> equipas = dados.getEquipas(); //nome, equipa
+        Map<Integer, Jogador> jogadores = dados.getJogadores(); //numero, jogador
+        List<EstadoJogo> jogos = dados.getJogos();
+
+        var equipaSet = equipas.entrySet();
+        
+        try{
+        File file = new File("dadosteste.txt");
+        
+        if(!file.exists()) file.createNewFile();
+        
+        PrintWriter print = new PrintWriter(file);
+        
+        for(var equipa : equipaSet){
+            print.println("Equipa:"+ equipa.getKey() +":\n");
+            Equipa EquipaValor = equipa.getValue();
+            List<Jogador> jogadoresEquipa = EquipaValor.getJogadores();
+            
+            for (Jogador jogador : jogadoresEquipa) {
+            print.println("Jogador:"+ jogador.getNome() +","
+                                    + jogador.getNrCamisola() +","
+                                    + jogador.getVelocidade() +","
+                                    + jogador.getResistencia() +","
+                                    + jogador.getDestreza() +","
+                                    + jogador.getImpulsao() +","
+                                    + jogador.getJogoCabeca() +","
+                                    + jogador.getRemate() +","
+                                    + jogador.getCapPasse() +"\n");
+            }
+            
+        }
+        
+        for(EstadoJogo jogo : jogos){
+            print.println("Jogo:"+ jogo.getEquipaCasa() +","
+                                 + jogo.getEquipaFora() +","
+                                 + jogo.getScoreCasa() +","
+                                 + jogo.getScoreFora() +","
+                                 + jogo.getData());
+                                 
+            List<Integer> jogadoresJogoC = jogo.getJogadoresCasa();
+            Map<Integer,Integer> subsCasa = jogo.getSubstituicoesCasa();
+            var subsCSet = subsCasa.entrySet();
+            List<Integer> jogadoresJogoF = jogo.getJogadoresFora();
+            Map<Integer,Integer> subsFora = jogo.getSubstituicoesFora();
+            var subsFSet = subsFora.entrySet();
+            
+            for(int jogCasa : jogadoresJogoC) {
+                print.println(jogCasa+",");
+            }
+            
+            for(var subCasa : subsCSet) {
+                print.println(subCasa.getKey() +"->"+ subCasa.getValue()+",");
+            }
+            
+            for(int jogFora : jogadoresJogoF) {
+                print.println(jogFora+",");
+            }
+            
+            for(var subFora : subsFSet) {
+                print.println(subFora.getKey() +"->"+ subFora.getValue()+",");
+            }
+        }
+        
+
+        print.close();
+        }    
+        catch(IOException e) {e.printStackTrace();}
     }
-    */
 }
